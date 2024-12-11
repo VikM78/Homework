fd5f44eb36a74a45509847c9262639979a0ffae1
VikM78 <vmakarov.kzn@gmail.com>
2024-12-11 18:46:38 +0300

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,8 @@
+count_of_homework = 12
+count_of_hours = 1.5
+name_of_kurs = 'Python'
+time_of_homework = count_of_homework / count_of_hours
+print("Курс:", name_of_kurs, ", всего задач:",
+      count_of_homework, ", затрачено часов:",
+      count_of_hours,", среднее время выполнения:", 
+      count_of_hours/count_of_homework)
