868f8ed8bccb3cf33df625b9561c936783c6c151
VikM78 <vmakarov.kzn@gmail.com>
2024-12-11 19:02:22 +0300

    Add files via upload

--- Variables.py
+++ Variables.py
@@ -4,5 +4,5 @@ name_of_kurs = 'Python'
 time_of_homework = count_of_homework / count_of_hours
 print("Курс:", name_of_kurs, ", всего задач:",
       count_of_homework, ", затрачено часов:",
-      count_of_hours,", среднее время выполнения:", 
+      count_of_hours,", среднее время выполнения:",
       count_of_hours/count_of_homework)
