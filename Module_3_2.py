a0d065251b70d99e364c60cc949d148ff3923edc
VikM78 <vmakarov.kzn@gmail.com>
2024-12-28 10:22:17 +0300

    Add files via upload

--- Module_3_2.py
+++ Module_3_2.py
@@ -7,7 +7,7 @@ def check_email(email_address=''):
             check_email = True
     return check_email
 
-def send_email(message, recipient, sender = 'university.help@gmail.com'):
+def send_email(message, recipient, *,sender = 'university.help@gmail.com'):
     send_email = True
     if not (check_email(recipient) and check_email(sender)):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
@@ -25,8 +25,8 @@ def send_email(message, recipient, sender = 'university.help@gmail.com'):
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
 
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender = 'urban.teacher@mail.uk')
 
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender = 'urban.teacher@mail.ru')
